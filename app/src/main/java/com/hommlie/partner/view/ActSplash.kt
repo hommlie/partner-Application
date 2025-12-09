fcf614eaac6cf7490e7c9b3bd0102c0f1761d4b4
Vishal Rathaur <vishalrathaur56@gmail.com>
2025-12-09 15:47:28 +0530

    09-12-2025

--- app/src/main/java/com/hommlie/partner/view/ActSplash.kt
+++ app/src/main/java/com/hommlie/partner/view/ActSplash.kt
@@ -280,11 +280,11 @@ class ActSplash : AppCompatActivity() {
                                     val versionCode = CommonMethods.getAppVersionCode(this@ActSplash)
                                     System.out.println("app version : "+versionCode)
                                     val currentAppVersion = state.data.versionCode
-//                                    if (currentAppVersion > versionCode) {
-//                                        binding.clUpdate.visibility = View.VISIBLE
-//                                    } else {
+                                    if (currentAppVersion > versionCode) {
+                                        binding.clUpdate.visibility = View.VISIBLE
+                                    } else {
                                         moveToNextScreen()
-//                                    }
+                                    }
                                 }else{
                                     moveToNextScreen()
                                 }
@@ -306,7 +306,5 @@ class ActSplash : AppCompatActivity() {
         super.onResume()
         viewModel.checkversion()
     }
-
-
 }
 
